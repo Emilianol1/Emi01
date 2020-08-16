323ebc6372709fa85b7100bb65b89d24a5545bba
Emilianol1 <65362464+Emilianol1@users.noreply.github.com>
2020-08-16 19:46:37 -0300

    CambioLogin
    1

--- js/init.js
+++ js/init.js
@@ -16,29 +16,31 @@ var hideSpinner = function(){
 }
 
 var getJSONData = function(url){
-    var result = {};
-    showSpinner();
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
+  var result = {};
+  showSpinner();
+  return fetch(url)
+  .then(response =>{
+    hideSpinner();
+    if(response.ok){
+      return response.json();
+    }else{
+      throw Error(response.statusText);
+    }
+  })
+  .then(function(response){
+        result.status = 'ok';
         hideSpinner();
+        result.data = response;
         return result;
-    });
+  })
+  .catch(function(error){
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
+  });
 }
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
--- js/products.js
+++ js/products.js
@@ -1,6 +1,46 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var show = [];
 
+function MostrarLista(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let Products = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + Products.imgSrc + `" alt="` + Products.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ Products.name + `</h4>
+                        <small class="text-muted">` + Products.soldCount + ` artículos vendidos</small>
+                    </div>
+                    <p>` + Products.description + `</p>
+                    <p>` + Products.currency +" " + Products.cost + ` </p>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            show = resultObj.data;
+            //Muestro las categorías ordenadas
+            MostrarLista(show);
+        }
+    });
 });
